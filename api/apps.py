c85ffa556f80216130bbd0fc3e22fb6bcd38ed1d
Fran1763 <franciscojavier.1763@gmail.com>
2025-08-12 19:32:30 +0200

    performance improved, ready method from apps changed

--- api/apps.py
+++ api/apps.py
@@ -1,28 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 from django.contrib.auth import get_user_model
-from django.db.utils import OperationalError, ProgrammingError
+
+def create_default_superuser(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(is_superuser=True).exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@admin.com',
+            password='admin123',
+            role='superadmin'
+        )
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
     
     def ready(self):
-        try:
-            CustomUser = get_user_model()
-            if not CustomUser.objects.filter(is_superuser=True).exists():
-                CustomUser.objects.create_superuser(
-                    username='admin',
-                    email='admin@admin.com',
-                    password='admin123',
-                    role='superadmin'
-                )
-                print("Superuser created successfully.\n" \
-                "Please change the password after logging in for the first time.    " \
-                "This is a default superuser account.\n" \
-                "You can log in with the following credentials:\n" \
-                    "Username: admin\n" \
-                    "Email: admin@admin.com\n" \
-                    "Password: admin123")
-        except (OperationalError, ProgrammingError):
-            # Base de datos aún no está lista (por ejemplo al correr 'migrate')
-            pass
+        post_migrate.connect(create_default_superuser, sender=self)
